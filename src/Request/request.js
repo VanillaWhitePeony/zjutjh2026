7f2c9b18f971968b1e4b654d0b6430f1ecc7c10f
plutobloom6 <plutobloom@qq.com>
2026-09-17 07:11:05 +0000

    添加 Request 模块

---
+++ src/Request/request.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import{useUserStore}from '@/store/user'
+
+const request=axios.create({
+    baseURL:'',//到时候再填
+    timeout:10000
+})
+
+request.interceptors.request.use(
+    (config)=>{
+        const userStore=useUserStore()
+        if (userStore.token) {
+            config.header.Authorization=`Bearer ${userStore.token}`
+        }
+        return config
+    },
+    (error)=>{
+        return Promise.reject(error)
+    }
+)
+
+request.interceptors.response.use(
+    (response)=>{
+        return response
+    },
+    (error)=>{
+        if (error.response?.status===401){
+            const userStore=useUserStore()
+            userStore.logout()
+            window.location.href='/login'
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default request
+//拦截什么的感觉还是搞得不是很清楚，这部分ai改了很多
